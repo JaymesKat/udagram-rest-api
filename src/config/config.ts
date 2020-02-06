8a768813f2259d1402c05add9a7b1c32d83e4436
James Katarikawe <james.katarikawe@andela.com>
2020-02-06 10:24:04 +0300

    Initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+export const config = {
+					dev: {
+						username: process.env.POSTGRES_DB_USERNAME,
+						password: process.env.POSTGRES_DB_PASS,
+						database: process.env.POSTGRES_DB_NAME,
+						host: process.env.POSTGRES_DB_HOST,
+						dialect: "postgres",
+						aws_region: process.env.AWS_REGION,
+						aws_profile: process.env.AWS_PROFILE,
+						aws_media_bucket: process.env.MEDIA_S3_BUCKET
+					},
+					prod: {
+						username: "",
+						password: "",
+						database: "udagram_prod",
+						host: "",
+						dialect: "postgres"
+					}
+				};
---
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -0,0 +1,98 @@
+import { Router, Request, Response } from 'express';
+import { FeedItem } from '../models/FeedItem';
+import { requireAuth } from '../../users/routes/auth.router';
+import * as AWS from '../../../../aws';
+
+const router: Router = Router();
+
+// Get all feed items
+router.get('/', async (req: Request, res: Response) => {
+    const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
+    items.rows.map((item) => {
+            if(item.url) {
+                item.url = AWS.getGetSignedUrl(item.url);
+            }
+    });
+    res.send(items);
+});
+
+//Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    const item = await FeedItem.findByPk(req.params.id);
+    if(!item){
+        res.status(404).send('Feed Item does not exist')
+    }
+    res.status(200).send(item)
+})
+// update a specific resource
+router.patch('/:id', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+
+        const item = await FeedItem.findByPk(req.params.id);
+        if (!item) {
+            res.status(404).send("Feed Item does not exist");
+        }
+
+        const { url, caption } = req.body;
+
+        if (!caption) {
+            return res.status(400).send({ message: "Invalid caption" });
+        }
+
+        if (!url) {
+            return res.status(400).send({ message: "Invalid file url" });
+        }
+
+        const feedItem = await new FeedItem({
+            caption: caption,
+            url
+        });
+
+        const saved_item = await feedItem.save();
+
+        saved_item.url = feedItem.url !== item.url ? AWS.getGetSignedUrl(saved_item.url) : feedItem.url; 
+        res.status(201).send(saved_item);
+});
+
+
+// Get a signed url to put a new item in the bucket
+router.get('/signed-url/:fileName', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+    let { fileName } = req.params;
+    const url = AWS.getPutSignedUrl(fileName);
+    res.status(201).send({url: url});
+});
+
+// Post meta data and the filename after a file is uploaded 
+// NOTE the file name is they key name in the s3 bucket.
+// body : {caption: string, fileName: string};
+router.post('/', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+    const caption = req.body.caption;
+    const fileName = req.body.url;
+
+    // check Caption is valid
+    if (!caption) {
+        return res.status(400).send({ message: 'Caption is required or malformed' });
+    }
+
+    // check Filename is valid
+    if (!fileName) {
+        return res.status(400).send({ message: 'File url is required' });
+    }
+
+    const item = await new FeedItem({
+            caption: caption,
+            url: fileName
+    });
+
+    const saved_item = await item.save();
+
+    saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+    res.status(201).send(saved_item);
+});
+
+export const FeedRouter: Router = router;
